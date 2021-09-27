fe2889318f2a4a0ee542cc16516f5100d7e5149f
nqg <nidaghuman@gmail.com>
2021-09-27 10:46:11 -0400

    separated styled components into separate file

--- src/features/restaurants/components/RestaurantInfo.js
+++ src/features/restaurants/components/RestaurantInfo.js
@@ -1,89 +1,70 @@
 import React from "react";
-import styled from "styled-components/native";
-import { Button, Card, Paragraph } from "react-native-paper";
+import { Text, View } from "react-native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
-
-
-//styled components
-const Title = styled.Text`
-fontFamily: ${props => props.theme.fonts.heading}
-fontSize: ${props => props.theme.fontSizes.body};
-color: ${props => props.theme.colors.ui.primary};
-`;
-const Info = styled.View`
-padding: ${props => props.theme.space[3]}
-`;
-const Rating = styled.View`
-flexDirection: row;
-paddingTop: ${props => props.theme.space[2]};
-paddingBottom: ${props => props.theme.space[2]};
-`;
-const Section = styled.View`
-flexDirection: row;
-alignItems: center;
-`
-const SectionEnd =styled.View`
-flexDirection: row;
-flex: 1;
-justifyContent: flex-end;
-`;
-
-const RestaurantCard = styled(Card)`
-  backgroundColor: ${props => props.theme.colors.bg.primary};
-`;
-const CardCover = styled(Card.Cover)`
-  padding: ${props => props.theme.space[3]};
-  backgroundColor: ${props => props.theme.colors.bg.primary};
-`;
-const Address = styled.Text`
-fontFamily: ${props => props.theme.fonts.body}
-fontSize: ${props => props.theme.fontSizes.caption};
-color: ${props => props.theme.colors.ui.primary};
-`
+import {
+  Title,
+  Info,
+  Rating,
+  Section,
+  SectionEnd,
+  Icon,
+  RestaurantCard,
+  CardCover,
+  Address,
+} from "./RestaurantInfoStyles";
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
-    //deconstruct the restaurant object
-    //(will get this info from the google API)
-    const {
-        name = "Some restaurant",
-        icon,
-        photos = [
-            "https://images.everydayhealth.com/images/diet-nutrition/34da4c4e-82c3-47d7-953d-121945eada1e00-giveitup-unhealthyfood.jpg?sfvrsn=a31d8d32_0",
-        ],
-        address = "100 street",
-        isOpenNow = true,
-        rating = 4,
-        isClosedTemporarily = false,
-    } = restaurant;
+  //deconstruct the restaurant object
+  //(will get this info from the google API)
+  const {
+    name = "Some restaurant",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://images.everydayhealth.com/images/diet-nutrition/34da4c4e-82c3-47d7-953d-121945eada1e00-giveitup-unhealthyfood.jpg?sfvrsn=a31d8d32_0",
+    ],
+    address = "100 street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+  } = restaurant;
 
-    //this will create an array with number of elements equal to the rating number
-    //Math.floor to account for decimal places
-    const ratingArray = Array.from(new Array(Math.floor(rating)))
+  //this will create an array with number of elements equal to the rating number
+  //Math.floor to account for decimal places
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
-    return (
-        <>
-            <RestaurantCard elevation={5}>
+  return (
+    <>
+      <RestaurantCard elevation={5}>
+        <CardCover source={photos} />
+        <Info>
+          <Title>{name}</Title>
+          <Section>
+            <Rating>
+              {ratingArray.map(() => (
+                <SvgXml xml={star} width={20} height={20} />
+              ))}
+            </Rating>
+            <SectionEnd>
+              {isClosedTemporarily && (
+                <Text style={{ paddingRight: 16, color: "red" }}>
+                  Closed Temporarily
+                </Text>
+              )}
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+              <View style={{ paddingLeft: 16 }}>
+                <Icon
+                  style={{ width: 16, height: 16 }}
+                  source={{ uri: icon }}
+                />
+              </View>
+            </SectionEnd>
+          </Section>
 
-                <CardCover source={photos} />
-                <Info>
-                    <Title>{name}</Title>
-                    <Section>
-                    <Rating>
-                        {ratingArray.map(() => (
-                        <SvgXml xml={star} width={20} height={20} />
-                        ))}
-                    </Rating>
-                    <SectionEnd>
-                    {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-                    </SectionEnd>
-                    </Section>
-                    
-
-                    <Address>{address}</Address>
-                </Info>
-            </RestaurantCard>
-        </>
-    );
+          <Address>{address}</Address>
+        </Info>
+      </RestaurantCard>
+    </>
+  );
 };
---
+++ src/features/restaurants/components/RestaurantInfoStyles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components/native";
+import {Card} from "react-native-paper";
+
+//styled components for info card
+export const Title = styled.Text`
+fontFamily: ${props => props.theme.fonts.heading}
+fontSize: ${props => props.theme.fontSizes.body};
+color: ${props => props.theme.colors.ui.primary};
+`;
+export const Info = styled.View`
+padding: ${props => props.theme.space[3]}
+`;
+export const Rating = styled.View`
+flexDirection: row;
+paddingTop: ${props => props.theme.space[2]};
+paddingBottom: ${props => props.theme.space[2]};
+`;
+export const Section = styled.View`
+flexDirection: row;
+alignItems: center;
+`
+export const SectionEnd =styled.View`
+flexDirection: row;
+flex: 1;
+justifyContent: flex-end;
+`;
+
+export const RestaurantCard = styled(Card)`
+  backgroundColor: ${props => props.theme.colors.bg.primary};
+`;
+export const CardCover = styled(Card.Cover)`
+  padding: ${props => props.theme.space[3]};
+  backgroundColor: ${props => props.theme.colors.bg.primary};
+`;
+export const Address = styled.Text`
+fontFamily: ${props => props.theme.fonts.body}
+fontSize: ${props => props.theme.fontSizes.caption};
+color: ${props => props.theme.colors.ui.primary};
+`
+
+export const Icon = styled.Image`
+width: ${props => props.theme.sizes[1]};
+height: ${props => props.theme.sizes[1]};
+
+`
