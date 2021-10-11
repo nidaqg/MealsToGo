d26b8e7aaefe5d408920ae1bc94eea1f0b78bfff
nqg <nidaghuman@gmail.com>
2021-10-11 10:43:12 -0400

    added location mocks

---
+++ src/services/location/locationService.js
@@ -0,0 +1,19 @@
+import camelize from "camelize";
+import {locations} from "./locationMock";
+
+export const locationRequest =() => {
+return new Promise((resolve, reject) => {
+const locationMock = locations[searchTerm];
+if(!locationMock){
+    reject("Not Found");
+}
+resolve(locationMock)
+})
+};
+
+export const locationTransform = (result) => {
+    const {geometry={}} = camelize(result.results)[0];
+    const {lat, lng} = geometry.location;
+
+    return {lat, lng};
+};
