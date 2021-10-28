6b632b89a0d1aec80476d2ec2b6a340f5420626f
nqg <nidaghuman@gmail.com>
2021-10-28 15:31:04 -0400

    added btn to camera screen

--- src/features/settings/screens/CameraScreen.js
+++ src/features/settings/screens/CameraScreen.js
@@ -1,51 +1,66 @@
-import React, {useRef, useState, useEffect, useContext} from 'react';
-import { Camera } from 'expo-camera';
-import { AuthContext } from '../../../services/authentication/AuthenticationContext';
-import styled from 'styled-components';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useRef, useState, useEffect, useContext } from "react";
+import { Camera } from "expo-camera";
+import { AuthContext } from "../../../services/authentication/AuthenticationContext";
+import styled from "styled-components";
+import { View, Text, TouchableOpacity } from "react-native";
 //async stores locally on the phone, no server needed
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
 const ProfileCamera = styled(Camera)`
-width: 100%;
-height: 100%;
-`
-export const CameraScreen = () => {
-    //have to get permissions from user to access camera
-    const [hasPermission, setHasPermission] = useState(null);
-    const cameraRef = useRef()
-    const {user} = useContext(AuthContext)
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  align-self: center;
+`;
 
-    //function to take picture
-    const snap = async () => {
-        if (cameraRef) {
-          const photo = await cameraRef.current.takePictureAsync();
-          console.log(photo);
-          AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
-        }
-      }
-    //check for camera permissions
-    useEffect(() => {
-        (async () => {
-          const { status } = await Camera.requestPermissionsAsync();
-          setHasPermission(status === 'granted');
-        })();
-      }, []);
-    
-      if (hasPermission === null) {
-        return <View />;
-      }
-      if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
-      }
+const CaptureBtn = styled(TouchableOpacity)`
+position: absolute;
+width: 70px;
+height: 70px;
+bottom: 20px;
+borderRadius: 50px;
+backgroundColor: red;
+`;
 
-    return (
-        <TouchableOpacity onPress={snap}>
-        <ProfileCamera
-        ref={camera => (cameraRef.current = camera)}
+
+export const CameraScreen = ({ navigation }) => {
+  //have to get permissions from user to access camera
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef();
+  const { user } = useContext(AuthContext);
+
+  //function to take picture
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
+  //check for camera permissions
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+      <ProfileCamera
+        ref={(camera) => (cameraRef.current = camera)}
         type={Camera.Constants.Type.front}
-        />
-       </TouchableOpacity>
-    )
-}
+      >
+      <CaptureBtn onPress={snap}>
+    </CaptureBtn>
+
+      </ProfileCamera>
+  );
+};
