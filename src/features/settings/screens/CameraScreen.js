e0954cc5ed456ba6c33859a44cdce3390bc5a72b
nqg <nidaghuman@gmail.com>
2021-10-28 15:05:04 -0400

    added camera functionality

--- src/features/settings/screens/CameraScreen.js
+++ src/features/settings/screens/CameraScreen.js
@@ -1,7 +1,11 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, {useRef, useState, useEffect, useContext} from 'react';
 import { Camera } from 'expo-camera';
+import { AuthContext } from '../../../services/authentication/AuthenticationContext';
 import styled from 'styled-components';
 import { View, Text, TouchableOpacity } from 'react-native';
+//async stores locally on the phone, no server needed
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 const ProfileCamera = styled(Camera)`
 width: 100%;
@@ -11,12 +15,14 @@ export const CameraScreen = () => {
     //have to get permissions from user to access camera
     const [hasPermission, setHasPermission] = useState(null);
     const cameraRef = useRef()
+    const {user} = useContext(AuthContext)
 
     //function to take picture
     const snap = async () => {
         if (cameraRef) {
           const photo = await cameraRef.current.takePictureAsync();
           console.log(photo);
+          AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
         }
       }
     //check for camera permissions
@@ -35,7 +41,7 @@ export const CameraScreen = () => {
       }
 
     return (
-        <TouchableOpacity>
+        <TouchableOpacity onPress={snap}>
         <ProfileCamera
         ref={camera => (cameraRef.current = camera)}
         type={Camera.Constants.Type.front}
--- src/features/settings/screens/SettingsScreen.js
+++ src/features/settings/screens/SettingsScreen.js
@@ -1,25 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../../services/authentication/AuthenticationContext";
 import { SafeArea } from "../../restaurants/components/SafeArea";
 import { Avatar, List } from "react-native-paper";
+import { useFocusEffect } from "@react-navigation/native";
 import { Text, View, TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export const SettingsScreen = ({ navigation }) => {
   const { onLogOut, user } = useContext(AuthContext);
+  const [photo, setPhoto] = useState(null);
+
+  //function for retrieving photo from async storage
+  const getProfilePic = async (currentUser) => {
+    const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+    setPhoto(photoUri);
+  };
+  
+  //use focusEffect hook to run function every time the screen is in focus
+  useFocusEffect(
+    React.useCallback(() => {
+    getProfilePic(user);
+  },[user]));
+
 
   return (
     <SafeArea>
-
-        <View style={{alignItems:'center', paddingTop:16}}>
-     <TouchableOpacity
-    onPress={()=> navigation.navigate("Camera")}
-     >
-      <Avatar.Icon 
-      size={180} 
-      icon="human" 
-      backgroundColor="#2182BD" 
-      />
-      </TouchableOpacity>
-      <Text style={{paddingTop:16}}>{user.email}</Text>
+      <View style={{ alignItems: "center", paddingTop: 16 }}>
+        <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+          {!photo && (
+            <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+          )}
+          {photo && (
+            <Avatar.Image
+              size={180}
+              source={{ uri: photo }}
+              backgroundColor="#2182BD"
+            />
+          )}
+        </TouchableOpacity>
+        <Text style={{ paddingTop: 16 }}>{user.email}</Text>
       </View>
 
       <List.Section>
